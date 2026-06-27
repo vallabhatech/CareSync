ed95a5518963c87359f0007587809c83b3768205
Surishetty Harivallabha Sai <vallabha1243@gmail.com>
2026-06-27 11:49:10 +0530

    Revert "Add voice assistant with speech recognition and navigation (#205)"
    This reverts commit 29ba78d5cbe09320c01790e4d1a9466913177fd8.

--- src/App.jsx
+++ src/App.jsx
@@ -43,8 +43,7 @@ import DosageCalculator from './pages/DosageCalculator';
 import HealthMetrics from './pages/HealthMetrics';
 import Footer from './components/Footer';
 import NotFound from './pages/NotFound';
-import VoiceAssistant from "./components/VoiceAssistant";
-import Telehealth from "./pages/Telehealth";
+import Telehealth from './pages/Telehealth';
 
 // Navigation targets. Labels are resolved at render time via i18n keys
 // (see the `nav` namespace) so the menu localises with the rest of the app.
@@ -322,7 +321,6 @@ function App() {
         </Routes>
       </div>
       <ScrollToTopButton />
-      <VoiceAssistant />
       <Footer />
     </AuthProvider>
   );
