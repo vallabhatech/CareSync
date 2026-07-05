adbae740ac0d4b803d2adcaa5bf7b3f44a29ae97
Rohan S Mirjankar <rohansm668@gmail.com>
2026-07-05 17:51:17 +0530

    use replaceAll() instead of replace()

--- src/utils/sanitize.js
+++ src/utils/sanitize.js
@@ -84,11 +84,11 @@ export function escapeCssForHtml(cssText) {
   }
 
   return String(cssText)
-    .replace(/&/g, '&amp;')
-    .replace(/</g, '&lt;')
-    .replace(/>/g, '&gt;')
-    .replace(/"/g, '&quot;')
-    .replace(/'/g, '&#39;');
+    .replaceAll('&', '&amp;')
+    .replaceAll('<', '&lt;')
+    .replaceAll('>', '&gt;')
+    .replaceAll('"', '&quot;')
+    .replaceAll("'", '&#39;');
 }
 
 /**
